8f1cb6b1c881723c8076b48ec2a735e94a9ae2c7
Ken Fehling <me@kenfehling.com>
2023-12-12 17:59:27 -1000

    output: export

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: 'export',  // GitHub Pages only supports using export
+  distDir: 'out',
 }
 
 module.exports = nextConfig
